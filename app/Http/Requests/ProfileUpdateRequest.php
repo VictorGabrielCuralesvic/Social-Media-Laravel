4549aae51876bd94b494400f7a03c8dca5f9ef80
VictorGabrielCuralesvic <miranhaomi@gmail.com>
2025-03-24 09:53:11 -0300

    Atualizado Update e Create do User
    - Foi implementado a criação de username e também a atualização do username do usuario

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -17,6 +17,7 @@ class ProfileUpdateRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255', 'not_regex:/\s/i'],
             'email' => [
                 'required',
                 'string',
@@ -25,6 +26,13 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'not_regex' => 'Username cannot have whitespaces'
         ];
     }
 }
